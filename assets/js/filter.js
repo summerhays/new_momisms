ae3dda9b1ea6e55e37c40f8526770862ba089f77
summerhays <32282359+summerhays@users.noreply.github.com>
2020-05-04 15:36:20 -0400

    Add files via upload

---
+++ assets/js/filter.js
@@ -0,0 +1,33 @@
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  button = div.getElementsByTagName("button");
+  for (i = 0; i < button.length; i++) {
+    txtValue = button[i].textContent || button[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      button[i].style.display = "";
+    } else {
+      button[i].style.display = "none";
+    button[i].className = button[i].className.replace("w3-grey", "w3-black");
+	    var z = button[i].nextElementSibling;
+    z.className = z.className.replace(" w3-show", "");
+	    }
+    }
+  }
+
+
+function openAccordion(id) {
+  var x = document.getElementById(id);
+  var y = document.getElementById(id).parentElement;
+  if (x.className.indexOf("w3-show") == -1/* && y.style.display == ""*/) {
+    x.className += " w3-show";
+    x.previousElementSibling.className =
+    x.previousElementSibling.className.replace("w3-black", "w3-grey");
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className =
+    x.previousElementSibling.className.replace("w3-grey", "w3-black");
+  }
+}
