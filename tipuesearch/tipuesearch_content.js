3e91efab8b5eb725bdb97d82e69cb2b9fff38964
summerhays <david@summerhays.net>
2021-11-13 15:04:03 -0500

    momism search update

--- tipuesearch/tipuesearch_content.js
+++ tipuesearch/tipuesearch_content.js
@@ -1,7 +1,7 @@
 ---
 ---
 var tipuesearch = {"pages": [
-{% for each in site.data.momisms %}
-  {"title": "{{ each.momism }}", "text": "{% if each.definition %}{{ each.definition }}. {% endif %}{% if each.example %}<em>{{ each.example }}. </em>{% endif %}{% if each.mommentary %}<strong>Mom: {{ each.mommentary }}. </strong>{% endif %}Momism ID # {{ each.order }}", "tags": "jQuery HTML5 CSS", "url": "/list.html#momism_id{{ each.order }}"},
+{% for each in site.data.mom-csv %}
+  {"title": `{{ each.momism }}`, "text": `{% if each.definition %}{{ each.definition }}{% endif %}{% if each.example %}<em>{{ each.example }}</em>{% endif %}{% if each.mommentary %}<strong>Mom: {{ each.mommentary }}. </strong>{% endif %}Momism ID # {{ each.order }}`, "tags": "jQuery HTML5 CSS", "url": `/dictionary/{{ each.order }}`},
 	{% endfor %}  
 ]};
